840773e366f4b4925df6e2782ac6bd26f668e4dc
siddharth dev <siddharthdev@siddharths-MacBook-Air.local>
2026-01-28 11:55:38 +0530

    02: authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme.provider";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import {dark} from "@clerk/themes"
 
 import "./globals.css";
 
@@ -26,6 +35,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider
+      appearance={{
+        theme: dark,
+      }}
+    >
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.variable} ${plexMono.variable} antialiased`}
@@ -36,9 +50,23 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <header>
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-rose-500 text-white p-2 rounded">
+                  Sign up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+              </SignedIn>
+          </header>
+           {children}
         </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
