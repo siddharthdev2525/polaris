ec452b37894c6cc6b09f5ca758f7cec47e920781
siddharth dev <siddharthdev@siddharths-MacBook-Air.local>
2026-02-08 18:21:10 +0530

    03: datababase setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme.provider";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
-import {dark} from "@clerk/themes"
 
+import { Providers } from "@/components/providers";
 import "./globals.css";
 
 const inter = Inter({
@@ -32,41 +24,18 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode; 
 }>) {
   return (
-    <ClerkProvider
-      appearance={{
-        theme: dark,
-      }}
-    >
+   
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.variable} ${plexMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <header>
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-rose-500 text-white p-2 rounded">
-                  Sign up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-              </SignedIn>
-          </header>
-           {children}
-        </ThemeProvider>
+          <Providers>
+              {children}
+          </Providers>
       </body>
     </html>
-    </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
+"use client"
+
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
 
 const X = () => {
+  const projects = useQuery(api.projects.get);
+  const createProject = useMutation(api.projects.create);
+
   return (
-    <div>
-      <Button> click me</Button>
+    <div className="flex flex-col gap-2 p-4">
+      <Button onClick={() => createProject({
+        name: "New project"
+      })}>
+        Add new
+      </Button>
+      {projects?.map((project) => (
+        <div className="border rounded p-2 flex flex-col" key={project._id}>
+          <p>{project.name}</p>
+          <p>Owner Id: {project.ownerId}</p>
+          </div>
+      ))}
     </div>
   )
-}
-
- export default X;
+};
+ 
+export default X;
